f6c939b354cfe3974d9644b930a811b5b7feb915
Mel <user@nb-infomm16-11.maths.ox.ac.uk>
2020-01-29 12:09:43 +0000

    basic files for graph_resgularised with parameter scanning loop

---
+++ Rank_k_MATLAB/examples/graph_regularised_demo.m
@@ -0,0 +1,7 @@
+%scripts to solve the graph regularised factorisation problem for binary matrices with feature information
+%Author: Mel Beckerleg
+%Date: Jan 2020 
+
+addpath('/home/user/Documents/Mel/Ethera/BMFC/Rank_k_MATLAB/source/scripts/features')
+
+MC_demo_solve_ADMM_paramscan
